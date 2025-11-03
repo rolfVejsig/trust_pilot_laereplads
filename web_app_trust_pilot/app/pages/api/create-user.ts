a3bbe227401cd11b67715ad5c8b00110756425cc
Ben Krüger <eaglebenraphael@web.de>
2025-11-03 12:36:52 +0100

    Change from page router to api router

--- web_app_trust_pilot/app/pages/api/create-user.ts
+++ web_app_trust_pilot/app/pages/api/create-user.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { GetConnection } from "./Connection";
+import { GetConnection } from "./connection"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
