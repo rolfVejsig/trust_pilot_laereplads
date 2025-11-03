94b8f2b530b8ddc7f3be4e808bd36d31618b5fe3
Ben Krüger <eaglebenraphael@web.de>
2025-11-03 12:37:11 +0100

    changed from pages to api router

--- web_app_trust_pilot/app/api/connection.ts
+++ web_app_trust_pilot/app/api/connection.ts
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 export async function GetConnection() {
     const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
+      host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
---
+++ web_app_trust_pilot/app/api/create-user/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { GetConnection } from "../connection"
+
+export async function POST(req: NextRequest) {
+  if (req.method !== "POST") {
+    return NextResponse.json(
+      { message: "Method not allowed"},
+      {status:405}
+    );
+  }
+
+  const { username, email, password } = await req.json();
+
+  if (!username || !email || !password) {
+    console.error("Missing required fields:", { username, email, password });
+    return NextResponse.json(
+      {
+        message: "Missing required fields",
+        received: { username, email, password },
+      },
+      {status: 400});
+  }
+
+  console.log("Request body is valid:", { username, email, password });
+
+  try {
+    const connection = await GetConnection();
+
+    const query = `CALL InsertUser(?, ?, ?, ?)`;
+    const values = [username, email, password, 1];
+
+    const [result] = await connection.execute(query, values);
+
+    await connection.end();
+
+    return NextResponse.json(
+      { message: "User created successfully" },
+      {status:200});
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      {status:500}
+    );
+  }
+}
